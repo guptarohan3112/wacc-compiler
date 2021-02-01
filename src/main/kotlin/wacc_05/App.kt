c14085844bae0e39edabd9058fb3692ea71b43fb
ras19 <ras19@ic.ac.uk>
2021-02-01 08:16:23 +0000

    [ras19] Added spacing in main file (name needs to be changed)

--- src/main/kotlin/wacc_05/App.kt
+++ src/main/kotlin/wacc_05/App.kt
@@ -15,8 +15,10 @@ object App {
         val inputStream = System.`in`
         val input = CharStreams.fromStream(inputStream)
         val lexer = BasicLexer(input)
+
         val tokens = CommonTokenStream(lexer)
         val parser = BasicParser(tokens)
+
         val tree = parser.prog()   //`prog` is the entry point we defined in our grammar
         println(tree.toStringTree(parser))
         println("FINISHED")
