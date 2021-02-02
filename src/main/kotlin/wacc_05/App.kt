7eb32d85b4024dc95fb62eceb19407969bc762c8
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-02-02 10:13:12 +0000

    [rg719] Implemented BaseErrorListener Class for syntax errors, modified structure of main App object Code to account for this, as well as moving main function to AppKt Class and adapting prints and comments for improving usability

--- src/main/kotlin/wacc_05/App.kt
+++ src/main/kotlin/wacc_05/App.kt
@@ -6,18 +6,31 @@ package wacc_05
 import antlr.BasicLexer
 import antlr.BasicParser
 import org.antlr.v4.runtime.*
+import kotlin.system.exitProcess
+
+fun main(args:Array<String>) {
+    App.runCompiler(args)
+}
 
 object App {
 
     @JvmStatic
-    fun main(args: Array<String>) {
-        println("YAYY")
+    fun runCompiler(args: Array<String>) {
+
+        println("Welcome to WACC: Please enter your input below")
+        println("Remember to click enter at the end of input in this temporary solution")
+
         val inputStream = System.`in`
         val input = CharStreams.fromStream(inputStream)
+
         val lexer = BasicLexer(input)
+        val errorListener = ErrorListener()
+        lexer.removeErrorListeners()   // remove default ConsoleErrorListener
+        lexer.addErrorListener(errorListener)
+
         val tokens = CommonTokenStream(lexer)
         val parser = BasicParser(tokens)
-        val tree = parser.prog()   //`prog` is the entry point we defined in our grammar
+        val tree = parser.prog()
         println(tree.toStringTree(parser))
         println("FINISHED")
     }
