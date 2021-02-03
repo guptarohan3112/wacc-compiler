d7a996199a0d6ec3b89cdcf049d3e76581e9e54a
Alex Usher <awu19@ic.ac.uk>
2021-02-03 14:17:46 +0000

    Further restructure the symbol table

--- src/main/kotlin/wacc_05/symbol_table/identifier_objects/ScopedIdentifierObject.kt
+++ src/main/kotlin/wacc_05/symbol_table/identifier_objects/ScopedIdentifierObject.kt
@@ -4,9 +4,5 @@ import main.kotlin.wacc_05.symbol_table.SymbolTable
 import main.kotlin.wacc_05.symbol_table.identifier_objects.IdentifierObject
 
 abstract class ScopedIdentifierObject(parentST: SymbolTable) : IdentifierObject() {
-    private val st : SymbolTable = SymbolTable(parentST, null)
-
-    init {
-        parentST.setEnclosed(st)
-    }
+    private val st : SymbolTable = SymbolTable(parentST)
 }
