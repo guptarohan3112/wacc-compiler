09bb19e213f806ae294f57197a40515e5164bde6
ras19 <ras19@ic.ac.uk>
2021-02-03 08:19:06 +0000

    [ras19] Removed TypeAST class (use string to look up type identifier in symtable)

--- src/main/kotlin/wacc_05/ast_structure/ParamAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/ParamAST.kt
@@ -1,6 +1,6 @@
 package wacc_05.ast_structure
 
-class ParamAST(val type: TypeAST,
+class ParamAST(val type: String,
                val name: String) : AST {
 
     override fun check() {
--- src/main/kotlin/wacc_05/ast_structure/TypeAST.kt
+++
@@ -1,5 +0,0 @@
-package wacc_05.ast_structure
-
-class TypeAST {
-
-}
