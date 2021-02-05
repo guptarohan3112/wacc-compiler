b6ba3fdecfcbe14e3dae22f91a1d5137a5673326
Alex Usher <awu19@ic.ac.uk>
2021-02-05 19:52:04 +0000

    [awu19] add the definition for an array elem

--- src/main/kotlin/wacc_05/ast_structure/assignment_ast/ArrayElemAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/assignment_ast/ArrayElemAST.kt
@@ -1,5 +1,11 @@
 package wacc_05.ast_structure.assignment_ast
 
-class ArrayElemAST {
+import wacc_05.ast_structure.AST
+import wacc_05.ast_structure.ExprAST
+
+class ArrayElemAST(private val ident : String, private val exprs : ArrayList<ExprAST>) : AST {
+    override fun check() {
+        TODO("Not yet implemented")
+    }
 
 }
