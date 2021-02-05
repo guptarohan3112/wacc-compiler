fbb658d08e6fd53f7d0b1724d4ca1376f82086a9
Alex Usher <awu19@ic.ac.uk>
2021-02-05 16:13:58 +0000

    [awu19 ] make correction to ProgramAST file

--- src/main/kotlin/wacc_05/ast_structure/ProgramAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/ProgramAST.kt
@@ -3,14 +3,9 @@ package wacc_05.ast_structure
 import java.util.*
 
 class ProgramAST(val functionList : ArrayList<FunctionAST>,
-                 val statementList : ArrayList<StatementAST>) : AST {
+                 val statementList : StatementAST) : AST {
 
     override fun check() {
 //        TODO("Not yet implemented")
-    }
-
-    override fun toString(): String {
-//        TODO("Not yet implemented")
-        return ""
     }
 }
