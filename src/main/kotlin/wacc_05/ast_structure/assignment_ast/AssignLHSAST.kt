30ad6ae39d2de9cf9389e395f9f35cc29600cb21
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-02-06 17:43:22 +0000

    [rg719] changed type of ArrayElemAST to be ExprAST.ArrayElemAST in AssignLHSAST.kt

--- src/main/kotlin/wacc_05/ast_structure/assignment_ast/AssignLHSAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/assignment_ast/AssignLHSAST.kt
@@ -1,14 +1,15 @@
 package wacc_05.ast_structure.assignment_ast
 
 import wacc_05.ast_structure.AST
+import wacc_05.ast_structure.ExprAST
 
 class AssignLHSAST(private val ident : String?) : AST {
     // we have to store these instead of making them extend AssignLHSAST
     // due to the possibility of ident
-    private var arrElem: ArrayElemAST? = null
+    private var arrElem: ExprAST.ArrayElemAST? = null
     private var pairElem: PairElemAST? = null
 
-    constructor(arrElem: ArrayElemAST) : this(null) {
+    constructor(arrElem: ExprAST.ArrayElemAST) : this(null) {
         this.arrElem = arrElem
     }
 
