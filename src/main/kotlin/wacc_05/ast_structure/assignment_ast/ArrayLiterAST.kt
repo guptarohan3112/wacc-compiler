759dc858746042a2237cc88a5a01428b06b6e3b3
ras19 <ras19@ic.ac.uk>
2021-02-08 18:00:27 +0000

    [ras19] Implemented getType method for all possible assign rhs ast nodes apart from functin calls (done already)

--- src/main/kotlin/wacc_05/ast_structure/assignment_ast/ArrayLiterAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/assignment_ast/ArrayLiterAST.kt
@@ -3,8 +3,15 @@ package wacc_05.ast_structure.assignment_ast
 import wacc_05.SemanticErrorHandler
 import wacc_05.ast_structure.ExprAST
 import wacc_05.symbol_table.SymbolTable
+import wacc_05.symbol_table.identifier_objects.TypeIdentifier
 
 class ArrayLiterAST(private val elems: ArrayList<ExprAST>) : AssignRHSAST() {
+
+    private lateinit var arrayType: TypeIdentifier
+
+    override fun getType(): TypeIdentifier {
+        return arrayType
+    }
 
     override fun check(st: SymbolTable, errorHandler: SemanticErrorHandler) {
         elems[0].check(st, errorHandler)
@@ -16,6 +23,7 @@ class ArrayLiterAST(private val elems: ArrayList<ExprAST>) : AssignRHSAST() {
                 errorHandler.typeMismatch(firstElemType, elems[i].getType())
             }
         }
+        arrayType = firstElemType
     }
 
 }
--- src/main/kotlin/wacc_05/ast_structure/assignment_ast/NewPairAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/assignment_ast/NewPairAST.kt
@@ -3,8 +3,15 @@ package wacc_05.ast_structure.assignment_ast
 import wacc_05.SemanticErrorHandler
 import wacc_05.ast_structure.ExprAST
 import wacc_05.symbol_table.SymbolTable
+import wacc_05.symbol_table.identifier_objects.TypeIdentifier
 
 class NewPairAST(private val fst: ExprAST, private val snd: ExprAST) : AssignRHSAST() {
+
+    private lateinit var elemType: TypeIdentifier
+
+    override fun getType(): TypeIdentifier {
+        return elemType
+    }
 
     override fun check(st: SymbolTable, errorHandler: SemanticErrorHandler) {
         fst.check(st, errorHandler)
@@ -12,6 +19,7 @@ class NewPairAST(private val fst: ExprAST, private val snd: ExprAST) : AssignRHS
         if (fst.getType() != snd.getType()) {
             errorHandler.pairTypeMismatch(fst.getType(), snd.getType())
         }
+        elemType = fst.getType()
     }
 
 }
