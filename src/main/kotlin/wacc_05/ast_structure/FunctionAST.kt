ffcb722151f1ed96a823c5bfc55e89ff20ec4f45
ras19 <ras19@ic.ac.uk>
2021-02-08 10:26:52 +0000

    [ras19] Changed FunctionAST to rely on semantic checking of type field

--- src/main/kotlin/wacc_05/ast_structure/FunctionAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/FunctionAST.kt
@@ -14,27 +14,32 @@ class FunctionAST(
     override fun check(st: SymbolTable, errorHandler: SemanticErrorHandler) {
 
         // look up return type in symbol table and check valid identifier
-        val returnType: IdentifierObject? = st.lookupAll(returnType.toString())
-        val func: IdentifierObject? = st.lookup(funcName)
+        returnType.check(st, errorHandler)
+//        val returnType: IdentifierObject? = st.lookupAll(returnType.toString())
 
         // assuming all types can be returned, do we need to check this?
-        if (returnType == null) {
-            errorHandler.invalidIdentifier(returnType.toString())
-        } else if (returnType !is TypeIdentifier) {
-            errorHandler.invalidType(returnType.toString())
-        } else if (func != null) {
-            // TODO do we need a specific repeatFunctionDeclaration? (I think this is fine)
+//        if (returnType == null) {
+//            errorHandler.invalidIdentifier(returnType.toString())
+//        } else if (returnType !is TypeIdentifier) {
+//            errorHandler.invalidType(returnType.toString())
+//        } else if (func != null) {
+//            errorHandler.repeatVariableDeclaration(funcName)
+//        }
+
+        val func: IdentifierObject? = st.lookup(funcName)
+        if (func != null) {
             errorHandler.repeatVariableDeclaration(funcName)
         }
 
         val funcST = SymbolTable(st)
+        val returnTypeIdent: IdentifierObject? = st.lookupAll(returnType.toString())
 
         val funcIdent =
-            FunctionIdentifier(funcName, returnType as TypeIdentifier, ArrayList(), funcST)
+            FunctionIdentifier(funcName, returnTypeIdent as TypeIdentifier, ArrayList(), funcST)
         st.add(funcName, funcIdent)
 
         // The return type is added as a explicit entry to the symbol table for the function scope
-        funcST.add(returnType.toString(), returnType)
+        funcST.add(returnType.toString(), returnTypeIdent)
 
         paramList?.check(funcST, errorHandler)
 
