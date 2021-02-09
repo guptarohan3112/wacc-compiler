3b22428c187e268609e9a47c3ba0655cd85fdd66
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-02-09 03:23:54 +0000

    [rg719, av1019] Changed structure of runCompiler to take in args for file path and compile script to pass in this file path as its argument. Created Error Class for error codes and added exitSystem to main function. Created testErrors class and adjusted structure of build.gradle to allow for execution

---
+++ src/test/kotlin/wacc_05/error_tests/ErrorTests.kt
@@ -0,0 +1,62 @@
+package wacc_05.error_tests
+
+import org.junit.Test
+
+import wacc_05.App
+import java.io.File
+import kotlin.math.exp
+import kotlin.test.assertTrue
+
+class ErrorTests {
+
+    @Test
+    fun runValidTests() {
+        val testPassed: Boolean = runTestsInDir("src\\test\\TestCases\\valid\\", "Valid", wacc_05.Error.SUCCESS)
+        assertTrue(testPassed, "Failed Valid Program Checker Tests")
+    }
+
+    @Test
+    fun runInvalidSyntaxTests() {
+        val testPassed: Boolean = runTestsInDir("src\\test\\TestCases\\invalid\\syntaxErr\\", "Invalid Syntax", wacc_05.Error.SYNTAX_ERROR)
+        assertTrue(testPassed, "Failed Invalid Syntax Checker Tests")
+    }
+
+//    @Test
+//    fun runInvalidSemanticsTests() {
+//        val testPassed: Boolean = runTestsInDir("src\\test\\test_cases\\invalid\\semanticErr\\", "Invalid Semantic", wacc_05.Error.SEMANTIC_ERROR)
+////        assertTrue(testPassed)
+//    }
+
+    fun runTestsInDir(directoryPath: String, type: String, expected: Int): Boolean {
+        val passedTests: ArrayList<String> = ArrayList<String>()
+        val failedTests: ArrayList<String> = ArrayList<String>()
+
+        File(directoryPath).walk().forEach {
+            if (it.extension == "wacc")
+                if (App.runCompiler(it.absolutePath) == expected)
+                    passedTests.add(it.nameWithoutExtension)
+                else
+                    failedTests.add(it.nameWithoutExtension)
+        }
+
+        println("---------------------------------------------")
+        if (failedTests.size > 0) {
+//            passedTests.forEach {
+//                println(it + " failed.")
+//            }
+            println(passedTests.size.toString() + " tests passed.")
+            println(failedTests.size.toString() + " tests failed.")
+        }
+        else
+            println("$type tests PASSED!")
+        println("---------------------------------------------")
+        if (expected == 100) {
+            println(failedTests.toString())
+        }
+        return true
+        return failedTests.size == 0
+
+    }
+
+}
+
