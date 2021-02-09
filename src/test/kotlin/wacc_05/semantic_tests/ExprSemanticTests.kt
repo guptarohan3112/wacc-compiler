fe14cfff63fc4ebca265caa026f6727e5bffba67
Alex Usher <awu19@ic.ac.uk>
2021-02-09 16:00:48 +0000

    [awu19] add the tests for ident expressions

--- src/test/kotlin/wacc_05/semantic_tests/ExprSemanticTests.kt
+++ src/test/kotlin/wacc_05/semantic_tests/ExprSemanticTests.kt
@@ -1,9 +1,12 @@
 package wacc_05.semantic_tests
 
-import io.mockk.mockk
+import io.mockk.*
+import org.junit.Test
 import wacc_05.SemanticErrorHandler
+import wacc_05.ast_structure.ExprAST
 import wacc_05.symbol_table.SymbolTable
 import wacc_05.symbol_table.identifier_objects.TypeIdentifier
+import wacc_05.symbol_table.identifier_objects.VariableIdentifier
 
 open class ExprSemanticTests {
 
@@ -13,4 +16,23 @@ open class ExprSemanticTests {
 
     val st: SymbolTable = SymbolTable(null)
     val seh: SemanticErrorHandler = mockk()
+
+    @Test
+    fun varIdentPresentCheck() {
+        st.add("int", intType)
+        st.add("x", VariableIdentifier("x", intType))
+
+        ExprAST.IdentAST("x").check(st, seh)
+    }
+
+    @Test
+    fun varIdentNotPresentCheck() {
+        st.add("int", intType)
+
+        every { seh.invalidIdentifier(any()) } just runs
+
+        ExprAST.IdentAST("x").check(st, seh)
+
+        verify(exactly = 1) { seh.invalidIdentifier("x") }
+    }
 }
