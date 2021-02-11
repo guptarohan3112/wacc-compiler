83a52de54e82942b8723d81e42aa97ee589a837b
Alex Usher <awu19@ic.ac.uk>
2021-02-11 10:31:30 +0000

    [awu19] fix the error tests runner

--- src/test/kotlin/wacc_05/error_tests/ErrorTests.kt
+++ src/test/kotlin/wacc_05/error_tests/ErrorTests.kt
@@ -10,51 +10,54 @@ class ErrorTests {
 
     @Test
     fun runValidTests() {
-        val testPassed: Boolean = runTestsInDir("src\\test\\TestCases\\valid\\", "Valid", wacc_05.Error.SUCCESS)
+        val testPassed: Boolean = runTestsInDir("src/test/test_cases/valid", "Valid", wacc_05.Error.SUCCESS)
         assertTrue(testPassed, "Failed Valid Program Checker Tests")
     }
 
     @Test
     fun runInvalidSyntaxTests() {
-        val testPassed: Boolean = runTestsInDir("src\\test\\TestCases\\invalid\\syntaxErr\\", "Invalid Syntax", wacc_05.Error.SYNTAX_ERROR)
+        val testPassed: Boolean =
+            runTestsInDir("src/test/test_cases/invalid/syntaxErr", "Invalid Syntax", wacc_05.Error.SYNTAX_ERROR)
         assertTrue(testPassed, "Failed Invalid Syntax Checker Tests")
     }
 
     @Test
     fun runInvalidSemanticsTests() {
-        val testPassed: Boolean = runTestsInDir("src\\test\\test_cases\\invalid\\semanticErr\\", "Invalid Semantic", wacc_05.Error.SEMANTIC_ERROR)
+        val testPassed: Boolean = runTestsInDir(
+            "src/test/test_cases/invalid/semanticErr",
+            "Invalid Semantic",
+            wacc_05.Error.SEMANTIC_ERROR
+        )
         assertTrue(testPassed, "Failed Invalid Semantic Checker Tests")
     }
 
     fun runTestsInDir(directoryPath: String, type: String, expected: Int): Boolean {
-        val passedTests: ArrayList<String> = ArrayList<String>()
-        val failedTests: ArrayList<String> = ArrayList<String>()
+        val passedTests: ArrayList<String> = ArrayList()
+        val failedTests: ArrayList<String> = ArrayList()
 
         File(directoryPath).walk().forEach {
             if (it.extension == "wacc") {
-                if (WaccCompiler.runCompiler(it.absolutePath) == expected)
-                    passedTests.add(it.nameWithoutExtension)
-                else
+                try {
+                    if (WaccCompiler.runCompiler(it.absolutePath) == expected)
+                        passedTests.add(it.nameWithoutExtension)
+                    else
+                        failedTests.add(it.nameWithoutExtension)
+                } catch (e: Exception) {
                     failedTests.add(it.nameWithoutExtension)
+                }
             }
         }
 
         println("---------------------------------------------")
-        if (failedTests.size > 0) {
 //            passedTests.forEach {
 //                println(it + " failed.")
 //            }
-            println(passedTests.size.toString() + " tests passed.")
-            println(failedTests.size.toString() + " tests failed.")
-        }
-        else
-            println("$type tests PASSED!")
+        println(passedTests.size.toString() + " tests passed.")
+        println(failedTests.size.toString() + " tests failed.")
         println("---------------------------------------------")
-        if (expected == 100) {
+        if (expected == 100 || expected == 200) {
             println(failedTests.toString())
         }
         return failedTests.size == 0
-
     }
-
 }
