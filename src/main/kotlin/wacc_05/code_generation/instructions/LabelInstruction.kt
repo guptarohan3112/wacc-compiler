fa74f9f2c01316e422152f134848aadd21d4e129
Alex Usher <awu19@ic.ac.uk>
2021-02-21 13:41:52 +0000

    [awu19] add a label generator for instances where we need to have unique labels for control flow

--- src/main/kotlin/wacc_05/code_generation/instructions/LabelInstruction.kt
+++ src/main/kotlin/wacc_05/code_generation/instructions/LabelInstruction.kt
@@ -1,8 +1,14 @@
 package wacc_05.code_generation.instructions
 
-//TODO: maybe add an option to just assign next label
-// e.g. if L0, L1 have been assigned, return "L2:"
-class LabelInstruction(private val name: String):Instruction {
+class LabelInstruction(private val name: String) : Instruction {
+
+    companion object {
+        private var currentLabel: Int = 0
+
+        public fun getUniqueLabel(): LabelInstruction {
+            return LabelInstruction("L${currentLabel++}")
+        }
+    }
 
     override fun toString(): String {
         return "$name:"
