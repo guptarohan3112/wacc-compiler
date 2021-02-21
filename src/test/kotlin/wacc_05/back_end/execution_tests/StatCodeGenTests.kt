3e6e699fe494510ddfabd599417e185c8e08c391
Apoorva Verma <apoorva.verma@hotmail.com>
2021-02-21 18:33:32 +0000

    [av1019] Create unit tests files for code generation in back-end tests.

---
+++ src/test/kotlin/wacc_05/back_end/execution_tests/StatCodeGenTests.kt
@@ -0,0 +1,18 @@
+package wacc_05.back_end.execution_tests
+
+import io.mockk.mockk
+import wacc_05.SemanticErrors
+import wacc_05.symbol_table.SymbolTable
+import wacc_05.symbol_table.identifier_objects.TypeIdentifier
+
+open class StatCodeGenTests {
+
+    val intType: TypeIdentifier.IntIdentifier = TypeIdentifier.INT_TYPE
+    val charType: TypeIdentifier.CharIdentifier = TypeIdentifier.CHAR_TYPE
+    val boolType: TypeIdentifier.BoolIdentifier = TypeIdentifier.BOOL_TYPE
+
+    var st: SymbolTable = SymbolTable(null)
+    var childSt: SymbolTable = SymbolTable(st)
+    var seh: SemanticErrors = mockk()
+
+}
