2756df3cc84f957ac58a373528f030e3af1b0ab2
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-02-22 21:48:53 +0000

    [rg719, av1019] Implemented classes for IOInstructions, adding .equals method for comparison and applyIO() base structure

--- src/main/kotlin/wacc_05/code_generation/AssemblyRepresentation.kt
+++ src/main/kotlin/wacc_05/code_generation/AssemblyRepresentation.kt
@@ -11,12 +11,12 @@ class AssemblyRepresentation() {
     // Instructions in the program, including those in user defined functions and the main function
     private val mainInstrs: ArrayList<Instruction>
     // IO functions that are called in the user defined program
-    private val ioInstrs: ArrayList<IOInstruction>
+    private val ioInstrs: HashSet<IOInstruction>
 
     init {
         dataInstrs = ArrayList()
         mainInstrs = ArrayList()
-        ioInstrs = ArrayList()
+        ioInstrs = HashSet()
     }
 
     // This function builds the '.s' file with the information stored in fields (after translation)
--- src/main/kotlin/wacc_05/code_generation/IOInstruction.kt
+++ src/main/kotlin/wacc_05/code_generation/IOInstruction.kt
@@ -4,6 +4,129 @@ import wacc_05.code_generation.instructions.Instruction
 
 sealed class IOInstruction {
 
-    abstract fun applyIO() : ArrayList<Instruction>
+    abstract fun applyIO(): ArrayList<Instruction>
+
+    //TODO: Remaining p_classes to be defined
+
+
+    class p_check_null_pointer : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_check_null_pointer
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+
+    }
+
+    class p_read_int : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_read_int
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
+
+    class p_print_ln() : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_print_ln
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
+
+    class p_throw_runtime_error() : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_throw_runtime_error
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
+
+    class p_print_int() : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_print_int
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
+
+    class p_print_string : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_print_string
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
+
+    data class p_divide_by_zero(val reg: Register) : IOInstruction() {
+
+        override fun applyIO(): ArrayList<Instruction> {
+            return arrayListOf(
+                // TODO: add internal representation instructions for this io instruction
+            )
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return other is p_divide_by_zero && this.reg.compareTo(other.reg) == 0
+        }
+
+        override fun hashCode(): Int {
+            return javaClass.hashCode()
+        }
+    }
 
 }
