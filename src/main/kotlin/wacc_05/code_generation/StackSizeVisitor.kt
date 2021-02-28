d27556fa2d514f3dcb2ea53ce5ce9add0b8500fd
ras19 <ras19@ic.ac.uk>
2021-02-28 09:53:59 +0000

    [ras19] Rearranged StackSizeVisitor and made it so it no longer extends the default visitor pattern

--- src/main/kotlin/wacc_05/code_generation/StackSizeVisitor.kt
+++ src/main/kotlin/wacc_05/code_generation/StackSizeVisitor.kt
@@ -1,11 +1,11 @@
 package wacc_05.code_generation
 
 import wacc_05.ast_structure.ASTBaseVisitor
-import wacc_05.ast_structure.FunctionAST
+//import wacc_05.ast_structure.FunctionAST
 import wacc_05.ast_structure.StatementAST
 import wacc_05.symbol_table.identifier_objects.TypeIdentifier
 
-class StackSizeVisitor : ASTBaseVisitor<Unit>() {
+class StackSizeVisitor /*: ASTBaseVisitor<Unit>()*/ {
 
     private var stackSize: Int = 0
 
@@ -15,6 +15,53 @@ class StackSizeVisitor : ASTBaseVisitor<Unit>() {
     val ARR_SIZE: Int = 4
     val STRING_SIZE: Int = 4
     val PAIR_SIZE: Int = 4
+
+    fun getStackSize(stat: StatementAST) : Int {
+        visitStat(stat)
+        return stackSize
+    }
+
+    private fun getSizeOfType(type: TypeIdentifier): Int {
+        return when (type) {
+            TypeIdentifier.INT_TYPE -> INT_SIZE
+            TypeIdentifier.BOOL_TYPE -> BOOL_SIZE
+            TypeIdentifier.CHAR_TYPE -> CHAR_SIZE
+            TypeIdentifier.STRING_TYPE -> STRING_SIZE
+            is TypeIdentifier.PairIdentifier -> PAIR_SIZE
+            is TypeIdentifier.ArrayIdentifier -> ARR_SIZE
+            else -> 0
+        }
+    }
+
+    fun visitStat(stat: StatementAST) {
+        return when (stat) {
+            is StatementAST.DeclAST -> {
+                val size: Int = getSizeOfType(stat.type.getType())
+                stackSize += size
+            }
+            is StatementAST.WhileAST -> {
+                visitStat(stat.body)
+            }
+            is StatementAST.SequentialAST -> {
+                visitStat(stat.stat1)
+                visitStat(stat.stat2)
+            }
+            is StatementAST.BeginAST -> {
+                visitStat(stat.stat)
+            }
+            is StatementAST.IfAST -> {
+                // Options:
+                // Allocate space on the stack for both branches at the beginning
+                // Determine which branch you will fall into and allocate stack space for that one branch
+                // Leave it up to the individual branch to deal with how much stack space they allocate (reference Compiler)
+                visitStat(stat.thenStat)
+                visitStat(stat.elseStat)
+            }
+            else -> {
+                return
+            }
+        }
+    }
 
 //    override fun visitBeginAST(begin: StatementAST.BeginAST) {
 //        visit(begin.stat)
@@ -70,47 +117,5 @@ class StackSizeVisitor : ASTBaseVisitor<Unit>() {
 //        val size: Int = getSizeOfType(decl.type.getType())
 //        stackSize += size
 //    }
-
-    private fun getSizeOfType(type: TypeIdentifier): Int {
-        return when (type) {
-            TypeIdentifier.INT_TYPE -> INT_SIZE
-            TypeIdentifier.BOOL_TYPE -> BOOL_SIZE
-            TypeIdentifier.CHAR_TYPE -> CHAR_SIZE
-            TypeIdentifier.STRING_TYPE -> STRING_SIZE
-            is TypeIdentifier.PairIdentifier -> PAIR_SIZE
-            is TypeIdentifier.ArrayIdentifier -> ARR_SIZE
-            else -> 0
-        }
-    }
-
-    fun visitStat(stat: StatementAST) {
-        return when (stat) {
-            is StatementAST.DeclAST -> {
-                val size: Int = getSizeOfType(stat.type.getType())
-                stackSize += size
-            }
-            is StatementAST.WhileAST -> {
-                visitStat(stat.body)
-            }
-            is StatementAST.SequentialAST -> {
-                visitStat(stat.stat1)
-                visitStat(stat.stat2)
-            }
-            is StatementAST.BeginAST -> {
-                visitStat(stat.stat)
-            }
-            is StatementAST.IfAST -> {
-                // Options:
-                // Allocate space on the stack for both branches at the beginning
-                // Determine which branch you will fall into and allocate stack space for that one branch
-                // Leave it up to the individual branch to deal with how much stack space they allocate (reference Compiler)
-                visitStat(stat.thenStat)
-                visitStat(stat.elseStat)
-            }
-            else -> {
-                return
-            }
-        }
-    }
 
 }
