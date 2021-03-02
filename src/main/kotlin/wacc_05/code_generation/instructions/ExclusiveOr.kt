49fabec3519e5759e9a2f685baea0533ff4c01be
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-03-02 14:19:59 +0000

    [rg719] Fixed formatting error in toString of EOR by adding commas

--- src/main/kotlin/wacc_05/code_generation/instructions/ExclusiveOr.kt
+++ src/main/kotlin/wacc_05/code_generation/instructions/ExclusiveOr.kt
@@ -6,7 +6,7 @@ import wacc_05.code_generation.Register
 class ExclusiveOr(private val dest: Register, private val op1: Operand, private val op2: Operand) : Instruction {
 
     override fun toString(): String {
-        return "EOR $dest $op1 $op2"
+        return "EOR $dest, $op1, $op2"
     }
 
 }
