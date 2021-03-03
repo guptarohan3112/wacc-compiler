551bcdd67845db69e4854c0f064364a7364948a9
ras19 <ras19@ic.ac.uk>
2021-03-03 10:41:38 +0000

    [ras19] Removed the allocation of stack space in inner if and while scopes within StackSizeVisitor

--- src/main/kotlin/wacc_05/code_generation/StackSizeVisitor.kt
+++ src/main/kotlin/wacc_05/code_generation/StackSizeVisitor.kt
@@ -18,20 +18,9 @@ class StackSizeVisitor {
                 val size: Int = stat.type.getType().getStackSize()
                 stackSize += size
             }
-            is StatementAST.WhileAST -> {
-                visitStat(stat.body)
-            }
             is StatementAST.SequentialAST -> {
                 visitStat(stat.stat1)
                 visitStat(stat.stat2)
-            }
-            is StatementAST.IfAST -> {
-                // Options:
-                // Allocate space on the stack for both branches at the beginning
-                // Determine which branch you will fall into and allocate stack space for that one branch
-                // Leave it up to the individual branch to deal with how much stack space they allocate (reference Compiler)
-                visitStat(stat.thenStat)
-                visitStat(stat.elseStat)
             }
             else -> {
                 return
