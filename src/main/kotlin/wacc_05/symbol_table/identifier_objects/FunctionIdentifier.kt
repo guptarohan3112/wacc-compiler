a461da3e7388397c782fa77c234b03933052312d
ras19 <ras19@ic.ac.uk>
2021-03-04 22:08:05 +0000

    [ras19] Removed symbol table field and getter in FunctionIdentifier

--- src/main/kotlin/wacc_05/symbol_table/identifier_objects/FunctionIdentifier.kt
+++ src/main/kotlin/wacc_05/symbol_table/identifier_objects/FunctionIdentifier.kt
@@ -4,15 +4,10 @@ import wacc_05.symbol_table.SymbolTable
 
 class FunctionIdentifier(
     private val returnType: TypeIdentifier,
-    private val params: ArrayList<ParamIdentifier>,
-    private val st: SymbolTable
+    private val params: ArrayList<ParamIdentifier>
 ) : IdentifierObject() {
 
     private var stackSize: Int = 0
-
-    fun getSymbolTable(): SymbolTable {
-        return st
-    }
 
     fun getReturnType(): TypeIdentifier {
         return returnType
