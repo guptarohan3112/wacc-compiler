4aa42966eee01a34520e8f6bbf4cd77d85a8ccdc
ras19 <ras19@ic.ac.uk>
2021-03-09 10:43:46 +0000

    [ras19] Implemented semantic checking in SemanticVisitor for ForAST

--- src/main/kotlin/wacc_05/front_end/SemanticVisitor.kt
+++ src/main/kotlin/wacc_05/front_end/SemanticVisitor.kt
@@ -104,7 +104,6 @@ class SemanticVisitor(
             errorHandler.repeatVariableDeclaration(decl.ctx, decl.varName)
         } else {
             // Check that right hand side and type of identifier match
-            decl.type.st = decl.st()
             val typeIdent: TypeIdentifier = decl.type.getType()
             visitChild(decl.st(), decl.assignment)
             val assignmentType: TypeIdentifier = decl.assignment.getType()
@@ -243,6 +242,36 @@ class SemanticVisitor(
 
             visitChild(bodySt, whileStat.body)
         }
+    }
+
+    override fun visitForAST(forLoop: StatementAST.ForAST) {
+        // Make sure that the first statement is a declaration
+        val forLoopST = SymbolTable(forLoop.st())
+        if (forLoop.decl !is StatementAST.DeclAST) {
+            errorHandler.invalidDeclaration(forLoop.ctx)
+        } else {
+            // Perform semantic checks on the declaration and ensure that the variable is of int type
+            visitChild(forLoopST, forLoop.decl)
+            if (forLoop.decl.type.getType() != TypeIdentifier.INT_TYPE) {
+                errorHandler.typeMismatch(forLoop.ctx, TypeIdentifier.INT_TYPE, forLoop.decl.type.getType())
+            }
+            // Does error recovery need to be done here?
+        }
+
+        // The looping expression must evaluate to a boolean
+        val loopingExpr: ExprAST = forLoop.loopExpr
+        visitChild(forLoopST, forLoop.loopExpr)
+        if (loopingExpr.getType() != TypeIdentifier.BOOL_TYPE) {
+            errorHandler.typeMismatch(
+                forLoop.ctx,
+                TypeIdentifier.BOOL_TYPE,
+                loopingExpr.getType()
+            )
+        } else {
+            // Perform semantic checks on the for loop body
+            visitChild(forLoopST, forLoop.body)
+        }
+
     }
 
     override fun visitIntLiterAST(liter: ExprAST.IntLiterAST) {
