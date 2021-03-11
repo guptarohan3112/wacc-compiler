f6e6119af4c7c6d2b4c47872422f8d2053ac139c
ras19 <ras19@ic.ac.uk>
2021-03-11 19:15:43 +0000

    [ras19, awu19] Included reference to GraphNode in AssignRHSAST.kt

--- src/main/kotlin/wacc_05/ast_structure/assignment_ast/AssignRHSAST.kt
+++ src/main/kotlin/wacc_05/ast_structure/assignment_ast/AssignRHSAST.kt
@@ -2,11 +2,14 @@ package wacc_05.ast_structure.assignment_ast
 
 import wacc_05.ast_structure.AST
 import wacc_05.code_generation.utilities.Register
+import wacc_05.graph_colouring.GraphNode
 import wacc_05.symbol_table.identifier_objects.TypeIdentifier
 
 abstract class AssignRHSAST : AST() {
 
     private var dest: Register? = null
+
+    private var graphNode: GraphNode? = null
 
     abstract fun getType(): TypeIdentifier
 
