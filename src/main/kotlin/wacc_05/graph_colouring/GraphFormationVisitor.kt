679626bdd3e07bcb92118e681ca525c2ff29e2bf
ras19 <ras19@ic.ac.uk>
2021-03-11 19:16:48 +0000

    [ras19, awu19] Added comments in GraphFormationVisitor for guidance on what needs to be implemented

--- src/main/kotlin/wacc_05/graph_colouring/GraphFormationVisitor.kt
+++ src/main/kotlin/wacc_05/graph_colouring/GraphFormationVisitor.kt
@@ -3,4 +3,8 @@ package wacc_05.graph_colouring
 import wacc_05.ast_structure.ASTBaseVisitor
 
 class GraphFormationVisitor(graph: InterferenceGraph): ASTBaseVisitor() {
+    // Visitor class which will make the interference graph (make all necessary nodes and put them
+    // in the list of nodes)
+
+    // Only need to worry about Assign (AssignRHS), Decl, Ident
 }
