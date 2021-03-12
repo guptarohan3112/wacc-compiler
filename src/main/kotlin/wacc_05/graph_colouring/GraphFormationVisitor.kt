b6f755311277b9fd99b450341a209ca68c53bc00
ras19 <ras19@ic.ac.uk>
2021-03-12 08:15:31 +0000

    [ras19] Started to implement visitor methods for declaration, assignment and identAST when forming the graph

--- src/main/kotlin/wacc_05/graph_colouring/GraphFormationVisitor.kt
+++ src/main/kotlin/wacc_05/graph_colouring/GraphFormationVisitor.kt
@@ -1,10 +1,30 @@
 package wacc_05.graph_colouring
 
 import wacc_05.ast_structure.ASTBaseVisitor
+import wacc_05.ast_structure.ExprAST
+import wacc_05.ast_structure.StatementAST
 
-class GraphFormationVisitor(graph: InterferenceGraph): ASTBaseVisitor() {
+class GraphFormationVisitor(private var graph: InterferenceGraph): ASTBaseVisitor() {
     // Visitor class which will make the interference graph (make all necessary nodes and put them
     // in the list of nodes)
 
-    // Only need to worry about Assign (AssignRHS), Decl, Ident
+    // int x = 3   0
+    override fun visitDeclAST(decl: StatementAST.DeclAST) {
+        visit(decl.assignment)
+        val graphNode = GraphNode(graph.getIndex())
+
+        graph.addNode(graphNode)
+        graph.incrementIndex()
+    }
+
+    override fun visitAssignAST(assign: StatementAST.AssignAST) {
+        visit(assign.rhs)
+        graph.incrementIndex()
+
+    }
+
+    override fun visitIdentAST(ident: ExprAST.IdentAST) {
+        val graphNode: GraphNode? = graph.findNode(ident.value)
+        graphNode?.updateEndIndex(graph.getIndex())
+    }
 }
