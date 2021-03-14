4529a4921bc9c0fdddf4df7afc6e50130e82f4bf
ras19 <ras19@ic.ac.uk>
2021-03-14 07:26:49 +0000

    [ras19] Changed stack size visitor to refer to the interference graph and only increment stacksize field when necessary

--- src/main/kotlin/wacc_05/code_generation/utilities/StackSizeVisitor.kt
+++ src/main/kotlin/wacc_05/code_generation/utilities/StackSizeVisitor.kt
@@ -1,29 +1,45 @@
 package wacc_05.code_generation.utilities
 
 import wacc_05.ast_structure.StatementAST
+import wacc_05.graph_colouring.GraphNode
+import wacc_05.graph_colouring.InterferenceGraph
 
 class StackSizeVisitor {
 
     private var stackSize: Int = 0
 
-    fun getStackSize(stat: StatementAST): Int {
-        visitStat(stat)
+//    fun getStackSize(graph: InterferenceGraph): Int {
+//        val nodes = graph.getNodes()
+//    }
+
+    fun getStackSize(stat: StatementAST, graph: InterferenceGraph): Int {
+        visitStat(stat, graph)
         return stackSize
     }
 
-    private fun visitStat(stat: StatementAST) {
+    private fun visitStat(stat: StatementAST, graph: InterferenceGraph) {
         return when (stat) {
             is StatementAST.DeclAST -> {
                 val size: Int = stat.type.getType().getStackSize()
-                stackSize += size
+                incrementStackSizeIfNecessary(stat, size, graph)
             }
             is StatementAST.SequentialAST -> {
-                visitStat(stat.stat1)
-                visitStat(stat.stat2)
+                visitStat(stat.stat1, graph)
+                visitStat(stat.stat2, graph)
             }
             else -> {
                 return
             }
         }
     }
+
+    private fun incrementStackSizeIfNecessary(stat: StatementAST.DeclAST, size: Int, graph: InterferenceGraph) {
+        val correctNode: GraphNode? = graph.findNode(stat.varName)
+        if (correctNode != null && !correctNode.getRegister().equals(InterferenceGraph.DefaultReg)) {
+            return
+        } else {
+            stackSize += size
+        }
+    }
+
 }
