60751b1b1a84baed984b2f552035432f652080d9
ras19 <ras19@ic.ac.uk>
2021-03-19 09:35:17 +0000

    [ras19] Changed range to classify an overlapping of live ranges for more correct graph colouring

--- src/main/kotlin/wacc_05/graph_colouring/GraphNode.kt
+++ src/main/kotlin/wacc_05/graph_colouring/GraphNode.kt
@@ -79,7 +79,7 @@ class GraphNode(private var startIndex: Int, private var ident: String = "") {
         val thatEnd: Int = node.getEndIndex()
 
         return if (thisStart < thatStart) {
-            thisEnd > thatStart
+            thisEnd >= thatStart
         } else {
             thatEnd > thisStart
         }
